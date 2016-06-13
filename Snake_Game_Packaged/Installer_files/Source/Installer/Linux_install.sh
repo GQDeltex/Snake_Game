b2ee48d4332dbf1eaa82a50d060e394ba03309e6
peter <operitum@gmail.com>
2016-06-13 15:24:48 +0200

    Changed Linux installer and recompiled the Snake-Game-Installer

---
+++ Snake_Game_Packaged/Installer_files/Source/Installer/Linux_install.sh
@@ -0,0 +1,1 @@
+sudo apt-get install python python-pygame libsdl1.2-dev && sudo apt-get autoremove
--- Snake_Game_Packaged/Installer_files/Source/Linux_install.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install python
-sudo apt-get install python-pygame
-sudo apt-get install libsdl1.2-dev
-
